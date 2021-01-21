1899c158cf51c7435891a5584c33eee0e2143fa9
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-01-21 16:53:04 -0700

    gallery

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,19 +1,19 @@
-// __tests__/About.test.js
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import About from '..';
+import React from 'react'
+import coverImage from "../../assets/cover/cover-image.jpg";
 
-afterEach(cleanup);
+function About() {
 
-describe('About component', () => {
-  it('renders', () => {
-    render(<About />);
-  });
-  
-  it('matches snapshot DOM node structure', () => {
-    const { asFragment } = render(<About />);
-    
-    expect(asFragment()).toMatchSnapshot();
-  });
-})
+  return (
+    <section className="my-5" >
+      <h1 id="about">Who am I?</h1>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+      </p>
+      </div>
+    </section>
+  )
+}
+
+export default About
